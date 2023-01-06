a7da43ee4d4a929156a41ba70fcd20281de4fe6b
Pavel Tyukin <tyukin.pavel@yandex.ru>
2023-01-06 13:57:32 +0300

    refactor: update and replace files

--- internal/server/server.go
+++ internal/server/server.go
@@ -10,22 +10,28 @@ type storage interface {
 	Set(mType, mName, mValue string)
 }
 
-func Serve(addr string, strg storage) error {
-	server := http.Server{
-		Addr: addr,
-	}
-
-	http.HandleFunc("/update/", func(w http.ResponseWriter, r *http.Request) {
+func createUpdateHandler(strg storage) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 		urlParts := strings.Split(r.RequestURI[8:], "/")
 		strg.Set(urlParts[0], urlParts[1], urlParts[2])
 
 		fmt.Println(strg)
 		w.WriteHeader(http.StatusOK)
-	})
+	}
+}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		w.WriteHeader(http.StatusNotFound)
-	})
+func createNotFoundHandler(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+}
+
+func Serve(addr string, strg storage) error {
+	server := http.Server{
+		Addr: addr,
+	}
+
+	http.HandleFunc("/update/", createUpdateHandler(strg))
+
+	http.HandleFunc("/", createNotFoundHandler)
 
 	return server.ListenAndServe()
 }
